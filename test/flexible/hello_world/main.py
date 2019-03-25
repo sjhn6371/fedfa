c632bae533fa08eacc7567e9d3f468fcc7b0ca4d
sjhn6371 <48905110+sjhn6371@users.noreply.github.com>
2019-03-25 17:52:54 +0900

    Update main.py

--- test/flexible/hello_world/main.py
+++ test/flexible/hello_world/main.py
@@ -15,7 +15,7 @@
 # [START gae_flex_quickstart]
 import logging
 
-from flask import Flask,, render_template, url_for, request
+from flask import Flask, render_template, url_for, request
 
 
 app = Flask(__name__)
