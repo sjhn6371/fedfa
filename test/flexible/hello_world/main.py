02206fc4f23fa781eb0fa0a074890303e0d90a61
sjhn6371 <48905110+sjhn6371@users.noreply.github.com>
2019-04-02 00:59:49 +0900

    Update main.py

--- test/flexible/hello_world/main.py
+++ test/flexible/hello_world/main.py
@@ -1,18 +1,4 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# [START gae_flex_quickstart]
 import logging
 
 from flask import Flask, render_template, url_for, request
@@ -25,6 +11,31 @@ app = Flask(__name__)
 def hello(name=None):
     """Return a friendly HTTP greeting."""
     return render_template("test.html", name=name)
+
+@app.route('/')
+@app.route('/calc/<num1>&<num2>&<ope>')
+def calc(num1,num2,ope):
+    a = int(num1)
+    b = int(num2)
+    if ope =='add':
+        c = a + b
+        ope = '+'
+        c = str(c)
+    if ope =='sub':
+        c = a - b
+        ope = '-'
+        c = str(c)
+    if ope =='multi':
+        c = a * b
+        ope = '×'
+        c = str(c)
+    if ope =='div':
+        c = a / b
+        ope = '÷'
+        c = str(c)
+    return render_template('test_2.html', num1=num1, num2=num2, ope=ope, out = c)
+
+
 
 
 @app.errorhandler(500)
