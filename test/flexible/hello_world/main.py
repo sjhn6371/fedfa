4a7a30dafca7d4a69550555ecb38084e7ecd3604
sjhn6371 <48905110+sjhn6371@users.noreply.github.com>
2019-04-16 02:50:30 +0900

    Update main.py

--- test/flexible/hello_world/main.py
+++ test/flexible/hello_world/main.py
@@ -95,17 +95,35 @@ def calc(num1,num2,ope):
     ca1 = calculator()
     return render_template('test_2.html', out = ca1.processing(num1, num2, ope))
 
- 
 @app.route('/upload2')
-def fileupload(num = None):
-    return render_template('upload2.html', num = num)
+def index():
+    return render_template('upload2.html')
 
-@app.route('/showupload',methods=['POST'])
-def showtext(num=None):
-    if request.method == 'POST':
-        temp = request.form['num']
-    return redirect(url_for('fileupload', num = temp))
-  
+@app.route('/upload', methods=['POST'])
+def upload():
+    """Process the uploaded file and upload it to Google Cloud Storage."""
+    uploaded_file = request.files.get('file')
+
+    if not uploaded_file:
+        return 'No file uploaded.', 400
+
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(uploaded_file.filename)
+
+    blob.upload_from_string(
+        uploaded_file.read(),
+        content_type=uploaded_file.content_type
+    )
+
+    # The public URL can be used to directly access the uploaded file via HTTP.
+    return blob.public_url
+
   
 @app.errorhandler(500)
 def server_error(e):
